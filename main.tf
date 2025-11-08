26aae5647551a6b95dfddd90ad3c2b99a9d85138
MaxJGruber <maxjgruber@gmail>
2025-11-08 12:22:33 +0100

    Configure public subnet

--- main.tf
+++ main.tf
@@ -28,3 +28,16 @@ resource "aws_internet_gateway" "igw" {
     Name = "main-igw"
   }
 }
+
+########################################
+# Public Subnet
+########################################
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
