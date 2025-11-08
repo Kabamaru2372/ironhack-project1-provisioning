0cb88756e32da273fa24509647485aecac5342ba
MaxJGruber <maxjgruber@gmail>
2025-11-08 12:49:11 +0100

    Configure private subnets

--- main.tf
+++ main.tf
@@ -30,8 +30,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 ########################################
-# Public Subnet
+# Subnets
 ########################################
+
+# Public
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -42,9 +44,34 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Private
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name = "private-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-central-1c"
+
+  tags = {
+    Name = "private-subnet-b"
+  }
+}
+
+
+
 ########################################
-# Route Table
+# Route Tables
 ########################################
+
+# Public
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main_vpc.id
 
@@ -60,4 +87,23 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public.id
+}
+
+
+# Private
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_subnet_b.id
+  route_table_id = aws_route_table.private.id
 }
