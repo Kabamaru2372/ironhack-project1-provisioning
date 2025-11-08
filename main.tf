c35b64bb970b0f78ae26983860bba9acc5401c03
MaxJGruber <maxjgruber@gmail>
2025-11-08 12:15:05 +0100

    Configure initial VPC

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@
 provider "aws" {
   region = "eu-central-1"
 }
+
+########################################
+# VPC
+########################################
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "project-1-main-vpc"
+  }
+}
