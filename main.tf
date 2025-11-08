ab2c195fe527bec6a743443ef8d9af09094a8509
MaxJGruber <maxjgruber@gmail>
2025-11-08 12:28:56 +0100

    Configure route table for public subnet

--- main.tf
+++ main.tf
@@ -41,3 +41,23 @@ resource "aws_subnet" "public_subnet" {
     Name = "public-subnet"
   }
 }
+
+########################################
+# Route Table
+########################################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
